743aeec748074e8581127904b365a78b09fc9413
Grigore Vieru <vieru.grigore896@gmail.com>
2022-12-13 12:46:43 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.listen(process.env.PORT || 3000, function () {
-    let date = new Date(Date.now());
-    let serverStartTime = date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-    console.log("Server was start/restart on port 3000: " + serverStartTime);
-});
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+//
+// app.use(express.static("public"));
+//
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+// });
+//
+// app.listen(process.env.PORT || 3000, function () {
+//     let date = new Date(Date.now());
+//     let serverStartTime = date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+//     console.log("Server was start/restart on port 3000: " + serverStartTime);
+// });
